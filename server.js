2b1eb44467e5272845445ad38f7770caccf58976
danyw <danielwiejak@gmai.com>
2023-02-25 09:29:58 +0000

    ex done

--- server.js
+++ server.js
@@ -32,53 +32,30 @@ const productModel = mongoose.model("product", productSchema);
 app.get("/", homeHandler);
 app.get("/productsapi", getProductsApiHandler);
 app.get("/product", getProductHandler);
+app.post("/addProduct", addProductHandler);
 
 // Routes Handlers
 function homeHandler(req, res) {
   res.send("Server is running");
 }
 async function getProductsApiHandler(req, res) {
-    let productsapi = await axios.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
-    res.status(200).send(productsapi.data);
+  let productsapi = await axios.get("http://makeup-api.herokuapp.com/api/v1/products.json");
+  res.status(200).send(productsapi.data);
+}
+
+async function addProductHandler(req, res) {
+  console.log(req.body);
+  const { name, brand, price, imageUrl, description } = req.body;
+
+  let newProduct = await productModel.create({ name, brand, price, imageUrl, description });
+  let allProducts = await productModel.find({});
+  res.send(allProducts);
 }
 
 async function getProductHandler(req, res) {
   let products = await productModel.find({});
   res.send(products);
 }
-
-function seedProductsCollection() {
-  const bronzer = new productModel({
-    name: "Maybelline Facestudio Master Contour Kit",
-    brand: "maybelline",
-    price: "15.99",
-    imageUrl: "https://d3t32hsnjxo7q6.cloudfront.net/i/4f731de249cbd4cb819ea7f5f4cfb5c3_ra,w158,h184_pa,w158,h184.png",
-    description:
-      "Maybelline Facestudio Master Contour Kit is the ultimate on the go all-in-one palette, with contouring brush included.  Define and highlight in a New York minute with this effortless 3-step face contouring kit.  This easy-to-use 3-step face contouring kit features a bronzer, blush and highlighter.",
-  });
-  const blush = new productModel({
-    name: "Maybelline Facestudio Master Blush",
-    brand: "maybelline",
-    price: "9.99",
-    imageUrl: "https://d3t32hsnjxo7q6.cloudfront.net/i/4f731de249cbd4cb819ea7f5f4cfb5c3_ra,w158,h184_pa,w158,h184.png",
-    description:
-      "Maybelline Facestudio Master Blush is the ultimate on the go all-in-one palette, with contouring brush included.  Define and highlight in a New York minute with this effortless 3-step face contouring kit.  This easy-to-use 3-step face contouring kit features a bronzer, blush and highlighter.",
-  });
-  const highlighter = new productModel({
-    name: "Maybelline Facestudio Master Highlight",
-    brand: "maybelline",
-    price: "9.99",
-    imageUrl: "https://d3t32hsnjxo7q6.cloudfront.net/i/4f731de249cbd4cb819ea7f5f4cfb5c3_ra,w158,h184_pa,w158,h184.png",
-    description:
-      "Maybelline Facestudio Master Highlight is the ultimate on the go all-in-one palette, with contouring brush included.  Define and highlight in a New York minute with this effortless 3-step face contouring kit.  This easy-to-use 3-step face contouring kit features a bronzer, blush and highlighter.",
-  });
-
-  bronzer.save();
-  blush.save();
-  highlighter.save();
-}
-
-// seedProductsCollection();
 
 app.listen(PORT, () => {
   console.log(`listening on PORT ${PORT}`);
